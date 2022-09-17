fa453ddced1f6c3e476762e4eead88b241f87910
Mr.Teeraphat Thongcam <weirkhnhlx2550@gmail.com>
2022-09-17 16:01:26 +0700

    add ping

---
+++ slashcommands/Infomation/ping.js
@@ -0,0 +1,21 @@
+const { EmbedBuilder, ApplicationCommandOptionType } = require('discord.js');
+
+module.exports = {
+    name: "ping",
+    description: "สถานะปิงของบอท",
+    botPermission: ["SEND_MESSAGES", "EMBED_LINKS", "CONNECT", "SPEAK"],
+    ownerOnly: false,
+
+    run: async (interaction, client) => {
+        await interaction.deferReply({ ephemeral: false });
+        const ping = new EmbedBuilder()
+            .setAuthor({ name: 'สถานะปิงของบอทในขณะนี้' })
+            .setDescription(`ขณะนี้ปิงของบอทอยู่ที่ ${client.ws.ping}ms `)
+            .setColor("#000080")
+            .setImage('https://cdn.discordapp.com/attachments/950770133972971558/1000924373684863056/standard_5.gif')
+            .setTimestamp()
+            .setFooter({ text: 'ขอบคุณผู้สนับสนุนโดย LSP-Hosting', iconURL: 'https://cdn.discordapp.com/attachments/961883426112286731/1013442567955812352/LSPJiBi-T.png' });
+
+        await interaction.editReply({ embeds: [ping] });
+    }
+};
