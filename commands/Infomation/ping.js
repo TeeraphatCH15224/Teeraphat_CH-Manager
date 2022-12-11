5abb2824e31d99a7ed430fe353759c86d4081b4e
TeeraphatCH15224 <weirkhnhlx2550@gmail.com>
2022-12-11 07:55:23 +0700

    Update ping.js

--- commands/Infomation/ping.js
+++ commands/Infomation/ping.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder,  } = require('discord.js');
 
 module.exports = {
     name: "ping",
@@ -16,6 +16,7 @@ module.exports = {
             .setImage('https://cdn.discordapp.com/attachments/950770133972971558/1000924373684863056/standard_5.gif')
             .setTimestamp()
             .setFooter({ text: 'ขอบคุณผู้สนับสนุนโดย LSP-Hosting', iconURL: 'https://cdn.discordapp.com/attachments/961883426112286731/1013442567955812352/LSPJiBi-T.png' });
+
 
         await message.channel.send({ embeds: [ping] });
     }
