93bb5ff8959243e4aeedd4af9f48f86caa46f749
TeeraphatCH15224 <weirkhnhlx2550@gmail.com>
2022-12-11 08:14:42 +0700

    Update Slashcommands New & Update Package.json

--- events/guild/interactionCreate.js
+++ events/guild/interactionCreate.js
@@ -1,34 +1,46 @@
-const { OWNER_ID } = require('../../settings/config.js');
-
-module.exports = async (client, interaction) => {
-    if (interaction.isCommand()) {
-        if (!client.slash.has(interaction.commandName)) return;
+module.exports = async(client, interaction) => {
+    if (interaction.isCommand || interaction.isContextMenuCommand || interaction.isModalSubmit || interaction.isChatInputCommand) {
         if (!interaction.guild) return;
-        const command = client.slash.get(interaction.commandName);
-        if (!command) return;
 
+        let subCommandName = "";
         try {
-            if (command.userPerms) {
-                if (!interaction.member.permissions.has(command.userPerms)) {
-                    return interaction.editReply({ content: `You don't have perm ${command.userPerms} to use this command!` });
-                }
-            }
-            if (command.botPerms) {
-                if (!interaction.guild.me.permissions.has(command.botPerms)) {
-                    return interaction.editReply({ content: `I don't have perm ${command.botPerms} to use this command!` });
-                }
-            }
-
-            if (command.ownerOnly) {
-                if (interaction.user.id !== OWNER_ID) {
-                    return interaction.editReply({ content: "You not owner the bot can't use this command!" });
-                }
-            }
-            command.run(interaction, client);
-
-        } catch (e) {
-            console.log(e)
-            await interaction.editReply({ content: "Something went wrong!", ephemeral: true });
+          subCommandName = interaction.options.getSubcommand();
+        } catch { };
+        let subCommandGroupName = "";
+        try {
+          subCommandGroupName = interaction.options.getSubcommandGroup();
+        } catch { };
+    
+        const command = client.slash.find(command => {
+          switch (command.name.length) {
+            case 1: return command.name[0] == interaction.commandName;
+            case 2: return command.name[0] == interaction.commandName && command.name[1] == subCommandName;
+            case 3: return command.name[0] == interaction.commandName && command.name[1] == subCommandGroupName && command.name[2] == subCommandName;
+          }
+        });
+    
+        if (!command) return;
+        if (!client.dev.includes(interaction.user.id) && client.dev.length > 0) { 
+            interaction.reply(`The bot is under maintenance. (Please come back again later)`); 
+            console.log(`[COMMAND] ${interaction.user.tag} trying request the command from ${interaction.guild.name} (${interaction.guild.id})`); 
+            return;
         }
+
+        const msg_cmd = [
+          `[COMMAND] ${command.name[0]}`,
+          `${command.name[1] || ""}`,
+          `${command.name[2] || ""}`,
+          `used by ${interaction.user.tag} from ${interaction.guild.name} (${interaction.guild.id})`,
+        ]
+
+        console.log(`${msg_cmd.join(" ")}`);
+
+    if (command) {
+        try {
+            command.run(client, interaction);
+        } catch (error) {
+            console.log(error)
+            await interaction.reply({ content: `Something went wrong!`, ephmeral: true });
+        }}
     }
 }
--- handlers/loadSlashCommand.js
+++ handlers/loadSlashCommand.js
@@ -1,14 +1,39 @@
-const { readdirSync } = require('fs');
+const chillout = require("chillout");
+const readdirRecursive = require("recursive-readdir");
+const { resolve, relative } = require("path");
 
 module.exports = async (client) => {
-    readdirSync("./slashcommands/").map(async dir => {
-        const commands = readdirSync(`./slashcommands/${dir}/`).map(async (cmd) => {
-            const pull = require(`../slashcommands/${dir}/${cmd}`)
-            client.slash.set(pull.name, pull);
-            if (pull.aliases) {
-                pull.aliases.map(x => client.slash.set(x, pull));
-            }
+    let interactionsPath = resolve("./slashcommands");
+    let interactionFiles = await readdirRecursive(interactionsPath);
+
+    await chillout.forEach(interactionFiles, (interactionFile) => {
+        const start = Date.now();
+        const rltPath = relative(__dirname, interactionFile);
+     //   console.log(`[INFO] Loading interaction at.. "${interactionFile}"`)
+        const command = require(interactionFile);
+
+        if (command.name.length > 3) {
+            console.log(`[WARN] "${rltPath}" The name list of the interaction file is too long. (>3) Skipping..`);
+            return;
+        }
+
+        if (!command.name?.length) {
+            console.log(`[WARN] "${rltPath}" The interaction file does not have a name. Skipping..`);
+            return;
+        }
+
+        if (client.slash.has(command.name)) {
+            console.log(`[WARN] "${command.name[1]}" interaction has already been installed. It's skipping.`)
+            return;
+        }
+
+        client.slash.set(command.name, command);
+     //   console.log(`[INFO] "${command.type == "CHAT_INPUT" ? `/${command.name.join(" ")}` : `${command.name[0]}`}" ${command.name[1] || ""}  ${command.name[2] || ""} The interaction has been uploaded. (it took ${Date.now() - start}ms)`);
         });
-    })
-    console.log(`[HANDLER - SLASH] SlashCommand Events Loaded`);
+
+        if (client.slash.size) {
+            console.log(`[INFO] ${client.slash.size} Interactions are Loaded!`);
+        } else {
+            console.log(`[WARN] No interactions loaded, is everything ok?`);
+        }
 }
